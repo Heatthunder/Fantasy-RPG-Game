5633df74bd4b4be1b1c46e6c42f0b3aa853c8224
Heatthunder <159095594+Heatthunder@users.noreply.github.com>
2025-06-24 20:00:44 -0700

    screwing around hehe

---
+++ test4.py
@@ -0,0 +1,36 @@
+# le_list = [1, 2, 3, 4, 5, 6, "hello", "goodbye"]
+# le_list [0]
+
+# e = {'name':'e', 'age': 10, ' height': 11}
+# e ['name']
+
+# while e['name'] == 'e':
+
+#     #do something
+
+#     e['name'] = 'EE'
+
+# counter = 0
+# while counter < 10:
+
+#     print (counter)
+#     counter += 1
+
+# tag = 0
+# while tag < 21:
+#     print (tag)
+#     tag += 2
+
+# Le_list = ["name:hi","age:30", "birthplace:that place other there, no right there, no stop looking in the opposite direction I'm pointing, look at where I'm pointing, NO NOT THERE, THERE!", "hair color: not grey but green"]
+# for C in Le_list:
+#     print (C)
+
+
+while True:
+    word = input("say one word:")
+    if word == "red":
+        print ("red")
+    if word == "blue":
+        print ("blue")
+    if word == "quit":
+        break
