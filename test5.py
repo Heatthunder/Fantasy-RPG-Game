58816a094968697eaf8d8e12c09ee1050e679094
Heatthunder <159095594+Heatthunder@users.noreply.github.com>
2025-07-10 20:28:46 -0700

    more object collision, background removal, etc

--- test5.py
+++ test5.py
@@ -7,7 +7,9 @@ screen = pygame.display.set_mode((500,300))
 
 clock = pygame.time.Clock()
 
-player_stats = {"health":100, "damage":1}
+sword_drawn = 0
+sword_sprite = pygame.Surface((1,1))
+player_stats = {"health":100, "damage":10}
 enemyA_stats = {"healther": 50, "damage":5}
 
 player_pos = [250, 150]
@@ -15,63 +17,71 @@ enemyA_pos = [170, 70]
 map_cords = [0, 0]
 
 while True:
-    
+
     map = pygame.image.load("assets\grasslands-concept-pixilart.png")
     mapB = pygame.transform.scale(map, (500, 300))
     # rectB = 
     screen.blit(mapB, map_cords)    
 
     bob = pygame.image.load("assets\sr5z75c92c220faws3.png")
-    # bob.set_colorkey ((0,0,0))
+    
     Bob = pygame.transform.scale(bob, (70, 60))
+    Bob.set_colorkey ((63,81,181))
     bob_box = pygame.Rect(player_pos[0],player_pos[1], 70, 60)
     rectA = (30,60,70,90)
     screen.blit(Bob, bob_box)
 
     weapon2 = pygame.image.load("assets\sr5zb291859ba3aws3.png")
+    weapon2.set_colorkey ((63,81,181))
     weapon2_down = pygame.transform.rotate(weapon2, 180)
+    weapon2_down.set_colorkey ((63,81,181))
     weapon2_right = pygame.transform.rotate(weapon2, -90)
+    weapon2_right.set_colorkey ((63,81,181))
     weapon2_left = pygame.transform.rotate(weapon2, 90)
+    weapon2_left.set_colorkey ((63,81,181))
 
     # Process player inputs.
     for event in pygame.event.get():
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_D = [player_pos[0] - event.pos[0], player_pos[1] - event.pos[1]]
             print(mouse_D)
+            sword_drawn = 10
             if mouse_D[0] < mouse_D[1] and mouse_D[0] > -1*mouse_D[1]:
                 weapon2B = pygame.Rect(player_pos[0] + 30, player_pos[1] - 30, 50,30)
-                screen.blit(weapon2, weapon2B)
+                sword_sprite = weapon2
             elif mouse_D[0] > mouse_D[1] and mouse_D[0] < -1*mouse_D[1]:
                 weapon2B = pygame.Rect(player_pos[0] + 30, player_pos[1] + 30, 50,30)
-                screen.blit(weapon2_down, weapon2B)
+                sword_sprite = weapon2_down
             elif mouse_D[1] > mouse_D[0] and mouse_D[1] < -1*mouse_D[0]:
                 weapon2B = pygame.Rect(player_pos[0] + 30, player_pos[1] + 30, 50,30)
-                screen.blit(weapon2_right, weapon2B)
+                sword_sprite = weapon2_right
             elif mouse_D[1] < mouse_D[0] and mouse_D[1] > -1*mouse_D[0]:
                 weapon2B = pygame.Rect(player_pos[0] - 30, player_pos[1] + 30, 50,30)
-                screen.blit(weapon2_left, weapon2B)
+                sword_sprite = weapon2_left
+        
         if event.type == pygame.QUIT:
             pygame.quit()
             raise SystemExit
-
+    
     # Do logical updates here.
     # ...
 
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_w]:
-            player_pos[1] -= 5 
-        if keys[pygame.K_s]:
-            player_pos[1] += 5 
-        if keys[pygame.K_a]:
-            player_pos[0] -= 5 
-        if keys[pygame.K_d]:
-            player_pos[0] += 5 
-        if keys[pygame.K_SPACE]:
-            player_stats["health"] -= 10
-            print(player_stats)
-        if player_stats["health"] == 0:
-            pygame.quit()
-            raise SystemExit
+        if sword_drawn == 0:
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_w]:
+                player_pos[1] -= 5 
+            if keys[pygame.K_s]:
+                player_pos[1] += 5 
+            if keys[pygame.K_a]:
+                player_pos[0] -= 5 
+            if keys[pygame.K_d]:
+                player_pos[0] += 5 
+            if keys[pygame.K_SPACE]:
+                player_stats["health"] -= 10
+                print(player_stats)
+            if player_stats["health"] == 0:
+                pygame.quit()
+                raise SystemExit
         
 
        # Fill the display with a solid color
@@ -93,48 +103,45 @@ while True:
 
 
     sally = pygame.image.load("assets\sr5z6cb376ca53aws3.png")
+    sally.set_colorkey ((63,81,181))
     sally_box = sally.get_rect()
-    sally_box.move_ip(100, 90)
+    sally_box.move_ip(97, 90)
     screen.blit(sally, sally_box)
     
-    if bob_box.colliderect(sally_box):
-        pygame.quit()
-        raise SystemExit
+    # if bob_box.colliderect(sally_box):
+    #     pygame.quit()
+    #     raise SystemExit
     
     enemyA = pygame.image.load("assets\sr5z70b2dbb43caws3.png")
     enemyA_box = pygame.Rect(enemyA_pos, (70, 60))
     EnemyA = pygame.transform.scale(enemyA, (70, 60))
+    EnemyA.set_colorkey ((63,81,181))
     screen.blit(EnemyA, enemyA_box)
 
-    # enemyA_D = [player_pos[0] - enemyA_pos[0], player_pos[1]- enemyA_pos[1]] # enemy direction logic, etc
-    # # enemyA_D = [0,0] #enemyA_D unneeded?
-    # # if player_pos[0] < enemyA_pos[0]:
-    # #     enemyA_D[0] = -1
-    # # elif player_pos[0] > enemyA_pos[0]:
-    # #     enemyA_D[0] = 1
-    # # elif player_pos[0] == enemyA_pos[0]:
-    # #     enemyA_D[0] = 0
-    # # if player_pos[1] > enemyA_pos[1]:
-    # #     enemyA_D[1] = -1
-    # # elif player_pos[1] > enemyA_pos[1]:
-    # #     enemyA_D[1] = 1
-    # # elif player_pos[1] == enemyA_pos[1]:
-    # #     enemyA_D[1] = 0
+    enemyA_D = [player_pos[0] - enemyA_pos[0], player_pos[1]- enemyA_pos[1]] # enemy direction logic, etc
     
-    # if enemyA_D[1] > 0:
-    #     enemyA_pos[1] += 1
-    # if enemyA_D[1] < 0:
-    #     enemyA_pos[1] -= 1
-    # if enemyA_pos[0] > 0:
-    #     enemyA_pos[0] += 1
-    # if enemyA_D[0] < 0:
-    #     enemyA_pos[0] -= 1
+    if enemyA_D[1] > 0:
+        enemyA_pos[1] += 1
+    if enemyA_D[1] < 0:
+        enemyA_pos[1] -= 1
+    if enemyA_D[0] > 0:
+        enemyA_pos[0] += 1
+    if enemyA_D[0] < 0:
+        enemyA_pos[0] -= 1
 
     weapon1 = pygame.image.load("assets\sr5z9a455fd099aws3.png")
+    weapon1.set_colorkey ((63,81,181))
     weapon1B = enemyA_box.move(50, 30)
     screen.blit(weapon1, weapon1B)
 
+    if sword_drawn > 0:
+            screen.blit(sword_sprite, weapon2B)
+            sword_drawn -= 1
+            if enemyA_box.colliderect(weapon2B):
+                enemyA_pos[0] = random.choice((100, 200, 300, 400, 500, 600))
+                enemyA_pos[1] = random.choice((100, 200, 300, 400, 500, 600))
+    
     pygame.display.flip()  # Refresh on-screen display
-    clock.tick(15)         # wait until next frame (at 60 FPS)
+    clock.tick(60)         # wait until next frame (at 60 FPS)
 
     # print(clock.tick)
